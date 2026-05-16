28b3ee56e62dc8df56887c77d2d53382fbd7e4aa
EstivenJaramilloGomez <149891748+Stivenjpa@users.noreply.github.com>
2026-05-15 21:44:02 -0500

    Actualizacion: Configuracion de conexion a firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp;
+  await Firebase.initializeApp();
+  print("Conexion a firebase: ${FirebaseAuth.instance.currentUser}");
   runApp(const MyApp());
 }
 
